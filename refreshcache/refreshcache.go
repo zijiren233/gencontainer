64325761736fbf7aed12c5beec86c263dbb98f69
zijiren233 <pyh1670605849@gmail.com>
2024-02-15 02:55:50 +0800

    Feat: refresh cache add cache error

--- refreshcache/refreshcache.go
+++ refreshcache/refreshcache.go
@@ -10,84 +10,93 @@ import (
 type RefreshFunc[T any, A any] func(ctx context.Context, args ...A) (T, error)
 
 type RefreshCache[T any, A any] struct {
-	refreshData *RefreshData[T, A]
-	refreshFunc atomic.Pointer[RefreshFunc[T, A]]
+	*RefreshData[T, A]
+	RefreshFunc atomic.Pointer[RefreshFunc[T, A]]
 }
 
-func NewRefreshCache[T any, A any](refreshFunc RefreshFunc[T, A], maxAge time.Duration) *RefreshCache[T, A] {
+func NewRefreshCache[T any, A any](refreshFunc RefreshFunc[T, A], maxAge time.Duration, opts ...RefreshDataOption[T, A]) *RefreshCache[T, A] {
 	c := RefreshCache[T, A]{
-		refreshData: NewRefreshData[T, A](maxAge),
+		RefreshData: NewRefreshData[T, A](maxAge, opts...),
 	}
 	c.SetRefreshFunc(refreshFunc)
 	return &c
 }
 
 func (r *RefreshCache[T, A]) GetRefreshFunc() RefreshFunc[T, A] {
-	return *r.refreshFunc.Load()
+	return *r.RefreshFunc.Load()
 }
 
 func (r *RefreshCache[T, A]) SetRefreshFunc(refreshFunc RefreshFunc[T, A]) {
-	r.refreshFunc.Store(&refreshFunc)
+	r.RefreshFunc.Store(&refreshFunc)
 }
 
 func (r *RefreshCache[T, A]) Get(ctx context.Context, args ...A) (data T, err error) {
-	return r.refreshData.Get(ctx, *r.refreshFunc.Load(), args...)
+	return r.RefreshData.Get(ctx, *r.RefreshFunc.Load(), args...)
 }
 
 func (r *RefreshCache[T, A]) Refresh(ctx context.Context, args ...A) (data T, err error) {
-	return r.refreshData.Refresh(ctx, *r.refreshFunc.Load(), args...)
-}
-
-func (r *RefreshCache[T, A]) Clear() {
-	r.refreshData.Clear()
-}
-
-func (r *RefreshCache[T, A]) Last() int64 {
-	return r.refreshData.Last()
-}
-
-func (r *RefreshCache[T, A]) LastTime() time.Time {
-	return r.refreshData.LastTime()
-}
-
-func (r *RefreshCache[T, A]) MaxAge() int64 {
-	return r.refreshData.MaxAge()
-}
-
-func (r *RefreshCache[T, A]) Raw() T {
-	return r.refreshData.Raw()
+	return r.RefreshData.Refresh(ctx, *r.RefreshFunc.Load(), args...)
 }
 
 func (r *RefreshCache[T, A]) Data() *RefreshData[T, A] {
-	return r.refreshData
+	return r.RefreshData
 }
 
 type RefreshData[T any, A any] struct {
-	last   int64
-	maxAge int64
-	lock   sync.Mutex
-	data   atomic.Pointer[T]
+	last    int64
+	maxAge  int64
+	lastErr int64
+	errAge  int64
+	err     atomic.Pointer[error]
+	lock    sync.Mutex
+	data    atomic.Pointer[T]
 }
 
-func NewRefreshData[T any, A any](maxAge time.Duration) *RefreshData[T, A] {
-	return &RefreshData[T, A]{
+type RefreshDataOption[T any, A any] func(*RefreshData[T, A])
+
+func WithErrAge[T any, A any](age time.Duration) RefreshDataOption[T, A] {
+	return func(r *RefreshData[T, A]) {
+		r.errAge = int64(age)
+	}
+}
+
+func NewRefreshData[T any, A any](maxAge time.Duration, opts ...RefreshDataOption[T, A]) *RefreshData[T, A] {
+	rd := &RefreshData[T, A]{
 		maxAge: int64(maxAge),
 	}
+	rd.data.Store(new(T))
+	rd.err.Store(new(error))
+	for _, opt := range opts {
+		opt(rd)
+	}
+	return rd
 }
 
 func (r *RefreshData[T, A]) Get(ctx context.Context, refreshFunc RefreshFunc[T, A], args ...A) (data T, err error) {
-	if (r.maxAge <= 0 && atomic.LoadInt64(&r.last) > 0) || (time.Now().UnixNano()-atomic.LoadInt64(&r.last) < r.maxAge) {
+	last := atomic.LoadInt64(&r.last)
+	if (r.maxAge < 0 && last > 0) || (time.Now().UnixNano()-last < r.maxAge) {
 		return *r.data.Load(), nil
+	}
+	if r.errAge > 0 && (time.Now().UnixNano()-atomic.LoadInt64(&r.lastErr) < r.errAge) {
+		return data, *r.err.Load()
 	}
 	r.lock.Lock()
 	defer r.lock.Unlock()
-	if (r.maxAge <= 0 && r.last > 0) || (time.Now().UnixNano()-r.last < r.maxAge) {
+	if (r.maxAge < 0 && r.last > 0) || (time.Now().UnixNano()-r.last < r.maxAge) {
 		return *r.data.Load(), nil
+	}
+	if r.errAge > 0 && (time.Now().UnixNano()-r.lastErr < r.errAge) {
+		return data, *r.err.Load()
 	}
 	defer func() {
 		if err == nil {
 			r.data.Store(&data)
 			atomic.StoreInt64(&r.last, time.Now().UnixNano())
+			atomic.StoreInt64(&r.lastErr, 0)
+		} else {
+			r.err.Store(&err)
+			atomic.StoreInt64(&r.lastErr, time.Now().UnixNano())
+			atomic.StoreInt64(&r.last, 0)
 		}
 	}()
 	return refreshFunc(ctx, args...)
@@ -100,6 +109,11 @@ func (r *RefreshData[T, A]) Refresh(ctx context.Context, refreshFunc RefreshFunc
 		if err == nil {
 			r.data.Store(&data)
 			atomic.StoreInt64(&r.last, time.Now().UnixNano())
+			atomic.StoreInt64(&r.lastErr, 0)
+		} else {
+			r.err.Store(&err)
+			atomic.StoreInt64(&r.lastErr, time.Now().UnixNano())
+			atomic.StoreInt64(&r.last, 0)
 		}
 	}()
 	return refreshFunc(ctx, args...)
@@ -117,10 +131,29 @@ func (r *RefreshData[T, A]) LastTime() time.Time {
 	return time.Unix(0, atomic.LoadInt64(&r.last))
 }
 
+func (r *RefreshData[T, A]) LastErr() int64 {
+	return atomic.LoadInt64(&r.lastErr)
+}
+
+func (r *RefreshData[T, A]) LastErrTime() time.Time {
+	return time.Unix(0, atomic.LoadInt64(&r.lastErr))
+}
+
 func (r *RefreshData[T, A]) MaxAge() int64 {
 	return r.maxAge
 }
 
-func (r *RefreshData[T, A]) Raw() T {
-	return *r.data.Load()
+func (r *RefreshData[T, A]) MaxErrAge() int64 {
+	return r.errAge
+}
+
+func (r *RefreshData[T, A]) Raw() (data T, err error) {
+	last := atomic.LoadInt64(&r.last)
+	if (r.maxAge < 0 && last > 0) || (time.Now().UnixNano()-last < r.maxAge) {
+		return *r.data.Load(), nil
+	}
+	if r.errAge > 0 && (time.Now().UnixNano()-atomic.LoadInt64(&r.lastErr) < r.errAge) {
+		return data, *r.err.Load()
+	}
+	return *r.data.Load(), *r.err.Load()
 }
--- refreshcache/refreshcache_test.go
+++ refreshcache/refreshcache_test.go
@@ -54,3 +54,33 @@ func TestRefreshData(t *testing.T) {
 	}))
 	fmt.Printf("LastTime: %v\n", d.LastTime())
 }
+
+func TestCacheError(t *testing.T) {
+	c := refreshcache.NewRefreshCache[int](func(context.Context, ...any) (int, error) {
+		fmt.Println("refresh")
+		return 0, fmt.Errorf("error")
+	}, time.Second, refreshcache.WithErrAge[int, any](time.Second*3))
+	fmt.Println(c.Get(context.Background()))
+	fmt.Println(c.Get(context.Background()))
+	time.Sleep(time.Second)
+	fmt.Println(c.Get(context.Background()))
+	time.Sleep(time.Second * 2)
+	fmt.Println(c.Get(context.Background()))
+}
+
+func TestRaw(t *testing.T) {
+	c := refreshcache.NewRefreshCache[int](func(context.Context, ...any) (int, error) {
+		return 3, nil
+	}, time.Second)
+	fmt.Println(c.Raw())
+	fmt.Println(c.Refresh(context.Background()))
+	fmt.Println(c.Raw())
+	fmt.Println(c.Data().Refresh(context.Background(), func(ctx context.Context, args ...any) (int, error) {
+		return 4, fmt.Errorf("error")
+	}))
+	fmt.Println(c.Raw())
+	fmt.Println(c.Data().Refresh(context.Background(), func(ctx context.Context, args ...any) (int, error) {
+		return 5, nil
+	}))
+	fmt.Println(c.Raw())
+}
